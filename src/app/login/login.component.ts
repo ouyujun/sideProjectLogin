82384f89aefc0b67d6bcb2899c99743d69fdba12
ouyujyun <ouyujyun@mail.com>
2022-03-31 16:44:00 +0800

    HW331:加入判斷

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,35 +9,54 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private mainService: MemberService,private router:Router) { }
+  constructor(private mainService: MemberService, private router: Router) { }
 
 
   input: string = "";
+  idHint: string = ""
+  pwHint: string = ""
   password: string = "";
 
 
   ngOnInit(): void {
   }
 
-  info:Member|null=null;
+  info: Member | null = null;
   onLogin() {
-    let suecess:boolean=false;
-    this.mainService.memberInfo.subscribe((res:Member[]) => {
-      res.map((list:Member) => {
-        console.log(list.id===this.input,list.passWord===this.password)
-        if(list.id==this.input && list.passWord==this.password){
-          suecess=true;
-          this.info=list;
-          this.router.navigate(['/']);
+    let suecess: boolean = false;
+    this.mainService.memberInfo.subscribe((res: Member[]) => {
+      res.map((list: Member) => {
+        if (list.id == this.input && list.passWord == this.password) {
+          suecess = true;
+          this.info = list;
+          this.router.navigate(['./page1']);
           return
+        } else {
+          if (!this.input && !this.password) {
+            this.pwHint = "";
+            this.idHint = "請輸入帳號與密碼!!";
+            return
+          } else if (!this.input) {
+            this.pwHint = "";
+            this.idHint = "請輸入帳號!!";
+            return
+          } else if (!this.password) {
+            this.idHint = "";
+            this.pwHint = "請輸入密碼!!";
+            return
+          } else {
+            this.pwHint = "";
+            this.idHint = "帳號或密碼輸入錯誤!!";
+          }
         }
       }
       )
     })
 
-    if(suecess){
-      localStorage.setItem("suecess",JSON.stringify(this.info));
-      
+    this.input = "";
+    this.password = "";
+    if (suecess) {
+      localStorage.setItem("suecess", JSON.stringify(this.info));
     }
   }
 }
